9597bf48028415eaa24c4f3a5fa027fc909fd152
zkdlin211 <yuhanyao9@gmail.com>
2022-12-07 15:54:46 -0500

    initial project structure

---
+++ cmall-coupon/src/test/java/com/cliffe/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliffe.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cmall-ware/src/main/java/com/cliffe/coupon/WareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cliffe.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WareServiceApplication.class, args);
+    }
+
+}
